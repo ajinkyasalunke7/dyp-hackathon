0981768c8c16ddaab1436854b6f1a9081db1eefe
surajc-spec <surajchougule378@gmail.com>
2025-04-05 22:49:33 +0530

    home commit

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-     extend: {
-       colors: {
-         primary: '#f0f4ff',    // light background
-         secondary: '#eef2ff', // features section bg
-         highlight: '#A294F9', // buttons, headings
-         accent: '#CDC1FF',    // footer bg
-       },
-     },
+      extend: {
+         colors: {
+            primary: "#f0f4ff", // light background
+            secondary: "#eef2ff", // features section bg
+            highlight: "#A294F9", // buttons, headings
+            accent: "#CDC1FF", // footer bg
+         },
+      },
    },
    plugins: [],
- };
- 
+};
